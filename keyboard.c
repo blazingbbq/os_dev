d844c02b25037aa8960c30b95cd940bda1a24ac4
BlazingBBQ <simon.cousineau99@gmail.com>
2020-05-07 19:12:11 -0400

    Added shift, ctrl and alt mod key support
    Holding shift will use alternate shifted ascii characters.
    Can register ctrl and alt key handlers

--- keyboard.c
+++ keyboard.c
@@ -4,8 +4,26 @@
 #include "util.h"
 
 key_handler_t key_handlers[NUM_SCANCODES];
+/* Can register combinations of mod keys by registering ctrl
+ * or alt key handler and checking for the other desired mods.
+ *
+ * Ctrl handlers take precedence over alt handlers when both
+ * keys are down.
+ */
+key_handler_t ctrl_key_handlers[NUM_SCANCODES];
+key_handler_t alt_key_handlers[NUM_SCANCODES];
 
-const char * ASCII_SCANCODES[] = {
+u8 keyboard_shift = 0;
+u8 keyboard_ctrl = 0;
+u8 keyboard_alt = 0;
+
+void toggle_shift(u8 scancode);
+void toggle_ctrl(u8 scancode);
+void toggle_alt(u8 scancode);
+
+void ctrl_q_handler(u8 scancode);
+
+const char * const ASCII_SCANCODES[] = {
   "", "ESC", "1", "2",
   "3", "4", "5", "6",
   "7", "8", "9", "0",
@@ -32,24 +50,76 @@ const char * ASCII_SCANCODES[] = {
   "", "ESC"
 };
 
+const char * const ASCII_SCANCODES_SHIFTED[] = {
+  "", "", "!", "@",
+  "#", "$", "%", "^",
+  "&", "*", "(", ")",
+  "_", "+", "", "",
+  "Q", "W", "E", "R",
+  "T", "Y", "U", "I",
+  "O", "P", "{", "}",
+  "", "", "A", "S",
+  "D", "F", "G", "H",
+  "J", "K", "L", ":",
+  "\"", "~", "", "|",
+  "Z", "X", "C", "V",
+  "B", "N", "M", "<",
+  ">", "?", "", "",
+  "", " ", "", "",
+  "", "", "", "",
+  "", "", "", "",
+  "", "", "", "",
+  "", "", "", "",
+  "", "", "", "",
+  "", "", "", "",
+  "", "", "", "",
+  "", "", "", "",
+  "", ""
+};
+
+
 u8 read_scan_code(void){
   return inb(KBD_DATA_PORT);
 }
 
 void keyboard_init() {
   register_interrupt_handler(KBD_INTERRUPT, keyboard_interrupt_handler);
+
+  /* Register mod key handlers */
+  register_key_handler(L_SHIFT_SCANCODE, toggle_shift);
+  register_key_handler(R_SHIFT_SCANCODE, toggle_shift);
+  register_key_handler(RELEASE_SCANCODE(L_SHIFT_SCANCODE), toggle_shift);
+  register_key_handler(RELEASE_SCANCODE(R_SHIFT_SCANCODE), toggle_shift);
+
+  register_key_handler(L_CTRL_SCANCODE, toggle_ctrl);
+  /* register_key_handler(R_CTRL_SCANCODE, toggle_ctrl); */
+  register_key_handler(RELEASE_SCANCODE(L_CTRL_SCANCODE), toggle_ctrl);
+  /* register_key_handler(RELEASE_SCANCODE(R_CTRL_SCANCODE), toggle_ctrl); */
+
+  register_key_handler(L_ALT_SCANCODE, toggle_alt);
+  /* register_key_handler(R_ALT_SCANCODE, toggle_alt); */
+  register_key_handler(RELEASE_SCANCODE(L_ALT_SCANCODE), toggle_alt);
+  /* register_key_handler(RELEASE_SCANCODE(R_ALT_SCANCODE), toggle_alt); */
+
+  register_ctrl_handler(0x10, ctrl_q_handler);
 }
 
 void keyboard_interrupt_handler(interrupt_state_t state) {
   (void) state;
   u8 scancode = read_scan_code();
 
-  if (key_handlers[scancode]) {
+  if (keyboard_ctrl && ctrl_key_handlers[scancode]) {
+    key_handler_t handler = ctrl_key_handlers[scancode];
+    handler(scancode);
+  } else if (keyboard_alt && alt_key_handlers[scancode]) {
+    key_handler_t handler = alt_key_handlers[scancode];
+    handler(scancode);
+  } else if (key_handlers[scancode]) {
     key_handler_t handler = key_handlers[scancode];
     handler(scancode);
-  } else if (scancode < RELEASE_SCANCODES) {
+  } else if (scancode < RELEASE_OFFSET) {
     char buf[16];
-    scancode_to_ascii(scancode, buf);
+    scancode_to_ascii(scancode, keyboard_shift, buf);
 
     fb_write(buf);
   }
@@ -59,62 +129,70 @@ void register_key_handler(u8 scancode, key_handler_t handler) {
   key_handlers[scancode] = handler;
 }
 
-void scancode_to_ascii(u8 scancode, char buf[]) {
+void register_ctrl_handler(u8 scancode, key_handler_t handler) {
+  ctrl_key_handlers[scancode] = handler;
+}
+
+void register_alt_handler(u8 scancode, key_handler_t handler) {
+  alt_key_handlers[scancode] = handler;
+}
+
+void scancode_to_ascii(u8 scancode, u8 shifted, char buf[]) {
   /* buf set but not used */
   (void) buf;
-  if (ASCII_SCANCODES[scancode]) {
-    strcpy(buf, ASCII_SCANCODES[scancode]);
+
+  if ((shifted)
+      ? ASCII_SCANCODES_SHIFTED[scancode]
+      : ASCII_SCANCODES[scancode]) {
+    strcpy(buf, (shifted)
+        ? ASCII_SCANCODES_SHIFTED[scancode]
+        : ASCII_SCANCODES[scancode]);
   }
-  /* switch (scancode) { */
-    /* case 0x01: */
-      /* strcpy(buf, "ESC"); */
-      /* break; */
-    /* case 0x02: */
-      /* strcpy(buf, "1"); */
-      /* break; */
-    /* case 0x03: */
-      /* strcpy(buf, "2"); */
-      /* break; */
-    /* case 0x04: */
-      /* strcpy(buf, "3"); */
-      /* break; */
-    /* case 0x05: */
-      /* strcpy(buf, "4"); */
-      /* break; */
-    /* case 0x06: */
-      /* strcpy(buf, "5"); */
-      /* break; */
-    /* case 0x07: */
-      /* strcpy(buf, "6"); */
-      /* break; */
-    /* case 0x08: */
-      /* strcpy(buf, "7"); */
-      /* break; */
-    /* case 0x09: */
-      /* strcpy(buf, "8"); */
-      /* break; */
-    /* case 0x0a: */
-      /* strcpy(buf, "9"); */
-      /* break; */
-    /* case 0x0b: */
-      /* strcpy(buf, "0"); */
-      /* break; */
-    /* case 0x0c: */
-      /* strcpy(buf, "-"); */
-      /* break; */
-    /* case 0x0d: */
-      /* strcpy(buf, "="); */
-      /* break; */
-    /* case 0x0e: */
-      /* strcpy(buf, "BCKSP"); */
-      /* break; */
-    /* case 0x0f: */
-      /* strcpy(buf, "TAB"); */
-      /* break; */
-/*  */
-    /* default: ; */
-      /* strcpy(buf, "default"); */
-      /* break; */
-  /* } */
+}
+
+void toggle_shift(u8 scancode) {
+  switch (scancode) {
+    case L_SHIFT_SCANCODE:
+    case R_SHIFT_SCANCODE:
+      keyboard_shift = 1;
+      break;
+    case RELEASE_SCANCODE(L_SHIFT_SCANCODE):
+    case RELEASE_SCANCODE(R_SHIFT_SCANCODE):
+      keyboard_shift = 0;
+      break;
+  }
+}
+
+void toggle_ctrl(u8 scancode) {
+  switch (scancode) {
+    case L_CTRL_SCANCODE:
+    /* case R_CTRL_SCANCODE: */
+      keyboard_ctrl = 1;
+      break;
+    case RELEASE_SCANCODE(L_CTRL_SCANCODE):
+    /* case RELEASE_SCANCODE(R_CTRL_SCANCODE): */
+      keyboard_ctrl = 0;
+      break;
+  }
+}
+
+void toggle_alt(u8 scancode) {
+  switch (scancode) {
+    case L_ALT_SCANCODE:
+    /* case R_ALT_SCANCODE: */
+      keyboard_alt = 1;
+      break;
+    case RELEASE_SCANCODE(L_ALT_SCANCODE):
+    /* case RELEASE_SCANCODE(R_ALT_SCANCODE): */
+      keyboard_alt = 0;
+      break;
+  }
+}
+
+void ctrl_q_handler(u8 scancode) {
+  (void) scancode;
+
+  /* TODO: exit(0) */
+  fb_write("CTRL+Q");
 }
 
--- keyboard.h
+++ keyboard.h
@@ -4,10 +4,18 @@
 #include "isr.h"
 #include "types.h"
 
-#define KBD_DATA_PORT       0x60
-#define KBD_INTERRUPT       33
-#define NUM_SCANCODES       0xFF
-#define RELEASE_SCANCODES   0x80
+#define KBD_DATA_PORT         0x60
+#define KBD_INTERRUPT         33
+#define NUM_SCANCODES         0xFF
+#define RELEASE_OFFSET        0x80
+#define RELEASE_SCANCODE(sc)  (sc + RELEASE_OFFSET)
+
+#define L_SHIFT_SCANCODE      0x2a
+#define R_SHIFT_SCANCODE      0x36
+#define L_CTRL_SCANCODE       0x1d
+#define R_CTRL_SCANCODE
+#define L_ALT_SCANCODE        0x38
+#define R_ALT_SCANCODE
 
 typedef void (*key_handler_t)(u8 scancode);
 
@@ -38,12 +46,29 @@ void keyboard_interrupt_handler(interrupt_state_t state);
  */
 void register_key_handler(u8 scancode, key_handler_t handler);
 
+/** register_ctrl_handler
+ *  Register key handler for specified scancode when ctrl is held
+ *
+ *  @param scancode   The scancode for which to register the handler
+ *  @param handler    The callback function to handle the scancode keypress
+ */
+void register_ctrl_handler(u8 scancode, key_handler_t handler);
+
+/** register_alt_handler
+ *  Register key handler for specified scancode when alt is held
+ *
+ *  @param scancode   The scancode for which to register the handler
+ *  @param handler    The callback function to handle the scancode keypress
+ */
+void register_alt_handler(u8 scancode, key_handler_t handler);
+
 /** scancode_to_ascii
  *  Convert keyboard scancode to ascii
  *
  *  @param scancode   Scancode to convert
+ *  @param shifted    Whether the shifted key value should be returned
  *  @param buf        Buffer to store converted value
  */
-void scancode_to_ascii(u8 scancode, char buf[]);
+void scancode_to_ascii(u8 scancode, u8 shifted, char buf[]);
 
 #endif /* INCLUDE_KEYBOARD_H */
